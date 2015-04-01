4dab4d76a96837f1921d9cdeca1ca29bf635c8cf
Roumen B. Antonov <roumen@roumen.us>
2015-04-01 12:24:11 -0700

    absolute paths in symlinks

--- zendywebhost/recipes/linkconfig.rb
+++ zendywebhost/recipes/linkconfig.rb
@@ -3,9 +3,8 @@
 script "linkconfigs" do
 		interpreter "bash"
 		user "root"
-		cwd "#{deploy[:deploy_to]}/current/"
 		code <<-EOH
-			ln -s #{deploy[:deploy_to]}/current/wp-configs/wp-config_zh_${Environment}.php #{deploy[:deploy_to]}/current/wp-config.php;
-			ln -s #{deploy[:deploy_to]}/shared/content #{deploy[:deploy_to]}/current/wp-content;
+			ln -s /srv/www/zh_wordpress/current/wp-configs/wp-config_zh_${Environment}.php /srv/www/zh_wordpress/current/wp-config.php;
+			ln -s /srv/www/zh_wordpress/shared/content /srv/www/zh_wordpress/current/wp-content;
 		EOH
 end
