867892c34faaf7fd5e25276cac4b16756c372347
Roumen B. Antonov <roumen@roumen.us>
2015-04-01 12:39:04 -0700

    absolute paths in symlinks 2

--- zendywebhost/recipes/linkconfig.rb
+++ zendywebhost/recipes/linkconfig.rb
@@ -1,10 +1,12 @@
 # the Environment variable is one of {dev|test|prod}
 
 script "linkconfigs" do
+Chef::Log.info("USER_ID: #{node[:deploy]['simplephpapp'][:environment_variables][:USER_ID]}")
 		interpreter "bash"
 		user "root"
 		code <<-EOH
-			ln -s /srv/www/zh_wordpress/current/wp-configs/wp-config_zh_${Environment}.php /srv/www/zh_wordpress/current/wp-config.php;
+			ln -s /srv/www/zh_wordpress/current/wp-configs/wp-config_zh_dev.php /srv/www/zh_wordpress/current/wp-config.php;
+			rm -rf /srv/www/zh_wordpress/current/wp-content;
 			ln -s /srv/www/zh_wordpress/shared/content /srv/www/zh_wordpress/current/wp-content;
 		EOH
 end
