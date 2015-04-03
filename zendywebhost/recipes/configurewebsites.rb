312ac26e8bf32b61f2f4ef791686f5eb74e76929
Roumen B. Antonov <roumen@roumen.us>
2015-04-03 15:39:04 -0700

    added primitive site config recipe

---
+++ zendywebhost/recipes/configurewebsites.rb
@@ -0,0 +1,9 @@
+script "configurewebsites" do
+		interpreter "bash"
+		user "root"
+		code <<-EOH
+				sed -i "/HessianServiceUrl/c\    define('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" /srv/www/zh_website/current/application/config/application.config.php ;
+				sed -i "/HessianServiceUrl/c\$this->AddRow('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" /srv/www/zh_website/current/app/data/setting.db.php ;
+				service httpd restart;
+		EOH
+end
