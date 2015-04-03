1b3a0fee1f3dbb87b76269d01f2d15ebc2aaf15f
Roumen B. Antonov <roumen@roumen.us>
2015-04-02 19:41:26 -0700

    added link to the ops directory recipe

--- zendywebhost/recipes/linkops.rb
+++ zendywebhost/recipes/linkops.rb
@@ -3,7 +3,7 @@ script "linkops" do
 		user "root"
 		code <<-EOH
 			    rm -rf /usr/share/tomcat7/ops;
-				ln -s /usr/share/tomcat7/webapps/zh_admin/ops /usr/share/tomcat7/;
+				ln -s /usr/share/tomcat7/webapps/zendyhealthapi/ops /usr/share/tomcat7/;
 				service tomcat7 restart;
 		EOH
 end
