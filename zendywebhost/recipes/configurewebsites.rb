961b39dcf742becf521c1391f1a54218189b0fa2
Roumen B. Antonov <roumen@roumen.us>
2015-04-03 18:29:06 -0700

    removed httpd restart

--- zendywebhost/recipes/configurewebsites.rb
+++ zendywebhost/recipes/configurewebsites.rb
@@ -4,6 +4,5 @@ script "configurewebsites" do
 		code <<-EOH
 				sed -i "/HessianServiceUrl/c\    define('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" /srv/www/zh_website/current/application/config/application.config.php ;
 				sed -i "/HessianServiceUrl/c\$this->AddRow('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" /srv/www/zh_admin/current/app/data/setting.db.php ;
-				service httpd restart;
 		EOH
 end
