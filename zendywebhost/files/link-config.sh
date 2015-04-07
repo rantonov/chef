a6606c43aad199d5599dfd9da5bbef6944b9b186
Roumen B. Antonov <roumen@roumen.us>
2015-04-06 20:00:21 -0700

    cleaned up the WP recipe in zendywebhost

--- zendywebhost/files/link-config.sh
+++
@@ -1,5 +0,0 @@
-#/usr/sh
-# the Environment variable is one of {dev|test|prod}
-ln -s /srv/www/zh_wordpress/current/wp-configs/wp-config_zh_${Environment}.php /srv/www/zh_wordpress/current/wp-config.php
-ln -s  /srv/www/zh_wordpress/shared/content /srv/www/zh_wordpress/current/wp-content
-
