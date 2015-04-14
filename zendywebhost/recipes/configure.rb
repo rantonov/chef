482bc9908fd1562cc7a0d229be397b597bce4f63
Roumen B. Antonov <roumen@roumen.us>
2015-04-13 23:52:11 -0700

    https

--- zendywebhost/recipes/configure.rb
+++ zendywebhost/recipes/configure.rb
@@ -94,22 +94,6 @@ node[:deploy].each do |app_name, deploy|
 					fi
 			EOH
 		end
-	elsif "#{deploy[:deploy_to]}".include? "website"
-		script "configurewebsite" do	
-			interpreter "bash"
-			user "root"
-			code <<-EOH
-					sed -i "/HessianServiceUrl/c\    define('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" #{deploy[:deploy_to]}/current/application/config/application.config.php ;
-			EOH
-		end
-	elsif "#{deploy[:deploy_to]}".include? "admin"
-		script "configureadmin" do
-			interpreter "bash"
-			user "root"
-			code <<-EOH
-					sed -i "/HessianServiceUrl/c\\$this->AddRow('HessianServiceUrl', 'http://internal-dev-api-lb-75921361.us-west-2.elb.amazonaws.com:8080/zendyhealthapi/services');" #{deploy[:deploy_to]}/current/app/data/setting.db.php ;
-			EOH
-		end	
 	end
 end
 
