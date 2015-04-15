4ec93e5fbc601ca4f4fab7f45f817c1bc878ba2a
Roumen B. Antonov <roumen@roumen.us>
2015-04-15 16:31:45 -0700

    memcached take 14

--- zendywebhost/recipes/installmemcache.rb
+++ zendywebhost/recipes/installmemcache.rb
@@ -1,11 +1,11 @@
-session_save_path="\'"
+session_save_path=""
 Chef::Log.info("********  Instances: #{node[:opsworks][:layers]['php-app'][:instances]} *************")
 
 
 node[:opsworks][:layers]['php-app'][:instances].each do | instance, conf |
 	session_save_path << "tcp://"<<"#{conf[:private_ip]}"<<":11211,"
 end
-session_save_path=session_save_path.chop<<"\'" #the last comma
+session_save_path=session_save_path.chop #the last comma
 
 Chef::Log.info("*********** memcache session.save_path=#{session_save_path}  *************")
 
@@ -38,14 +38,14 @@ script "installmemcache" do
 		echo '*********** configure php to use memcached for sessions *************'
 		if grep "^\ *session.save_path" /etc/php5/apache2/php.ini ; then
 			echo '*********** the line exists *************'
-			cat /etc/php5/apache2/php.ini | sed "/^session.save_path/c\ session.save_path=#{session_save_path}" > /etc/php5/apache2/php.ini.new;
-			cp /etc/php5/apache2/php.ini.new /etc/php5/apache2/php.ini;
+			cat /etc/php5/apache2/php.ini | sed "/^\ *session.save_path/c\ session.save_path=\'#{session_save_path}\'" > /etc/php5/apache2/php.ini.new;
 		else
 			echo '*********** the line is new *************'
-			cat /etc/php5/apache2/php.ini |sed "/^;.*session.save_path = \"N;\/path/c\ session.save_path=#{session_save_path}" > /etc/php5/apache2/php.ini.new;
-			cp /etc/php5/apache2/php.ini.new /etc/php5/apache2/php.ini;
+			cat /etc/php5/apache2/php.ini |sed "/; The path can be defined as:/c\ session.save_path=\'#{session_save_path}\'" > /etc/php5/apache2/php.ini.new;
 		fi
-		
+		cp /etc/php5/apache2/php.ini.new /etc/php5/apache2/php.ini;
+
+	
 		cat /etc/php5/apache2/php.ini | sed "/session.save_handler/c\  session.save_handler = memcache" > /etc/php5/apache2/php.ini.new;
 		cp /etc/php5/apache2/php.ini.new /etc/php5/apache2/php.ini;
 				 
