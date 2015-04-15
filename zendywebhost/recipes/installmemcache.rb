9ce60e08d942edd0f337aa77ac0af2f5568a8182
Roumen B. Antonov <roumen@roumen.us>
2015-04-15 14:54:46 -0700

    memcached take 13

--- zendywebhost/recipes/installmemcache.rb
+++ zendywebhost/recipes/installmemcache.rb
@@ -25,20 +25,6 @@ script "installmemcache" do
 		echo '*********** restarting memcached *************'
 		service memcached restart;
 		
-	
-		echo '*********** configure php to use memcached for sessions *************'
-		echo sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
-		sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
-		
-		if grep "^\ *session.save_path" /etc/php5/apache2/php.ini ; then
-			echo '*********** the line exists *************'
-			sed -i "/^session.save_path/c\ session.save_path=#{session_save_path}" /etc/php5/apache2/php.ini;
-		else
-			echo '*********** the line is new *************'
-			sed -i "/^;.*session.save_path = \"N;\/path/c\ session.save_path=#{session_save_path}" /etc/php5/apache2/php.ini;
-		fi
-
-		 
 		echo '*********** configure redundancy *************'
 		if ! grep "memcache\.allow_failover=1" /etc/php5/mods-available/memcache.ini ;  then
 			echo "memcache.allow_failover=1" >> /etc/php5/mods-available/memcache.ini;
@@ -47,5 +33,17 @@ script "installmemcache" do
 			sed -i "/memcache.session_redundancy/c\  memcache.session_redundancy=#{node[:opsworks][:layers]['php-app'][:instances].length+1}" /etc/php5/mods-available/memcache.ini;
 		fi
 		
+	
+		echo '*********** configure php to use memcached for sessions *************'
+		if grep "^\ *session.save_path" /etc/php5/apache2/php.ini ; then
+			echo '*********** the line exists *************'
+			sed -i "/^session.save_path/c\ session.save_path=#{session_save_path}" /etc/php5/apache2/php.ini;
+		else
+			echo '*********** the line is new *************'
+			sed -i "/^;.*session.save_path = \"N;\/path/c\ session.save_path=#{session_save_path}" /etc/php5/apache2/php.ini;
+		fi
+		echo sed -i \"/session.save_handler/c\ session.save_handler = memcache\" /etc/php5/apache2/php.ini;
+		sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
+				 
 	EOH
 end
