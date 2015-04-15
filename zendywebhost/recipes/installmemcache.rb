d96260560023f99744b40eabd86b4c23c3b18912
Roumen B. Antonov <roumen@roumen.us>
2015-04-15 12:03:30 -0700

    memcached take 6

--- zendywebhost/recipes/installmemcache.rb
+++ zendywebhost/recipes/installmemcache.rb
@@ -1,13 +1,41 @@
-script "variousfiles" do
+
+session_save_path=""
+
+node[:opsworks][:layers]['php-app'][:instances].each do | instance |
+	session_save_path << "tcp://"<<"#{instance[:private_ip].to_s}"<<":11211,"
+end
+session_save_path=session_save_path.chop #the last comma
+
+Chef::Log.info("*********** memcache session.save_path=#{session_save_path}  *************")
+
+script "installmemcache" do
 	interpreter "bash"
 	user "root"
 	code <<-EOH
 		apt-get -y install php5-memcache memcached;
 		# configure memcached
+		# change the server ip
 		cat /etc/memcached.conf  | sed "s/127\.0\.0\.1/`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'`/" > /etc/memcached.conf.new;
 		mv /etc/memcached.conf.new /etc/memcached.conf;
 		service memcached restart;
-		echo "memcache.allow_failover=1" >> /etc/php5/mods-available/memcache.ini;
-		echo memcache.session_redundancy=#{node[:opsworks][:layers]['php-app'][:instances].length+1} >> /etc/php5/mods-available/memcache.ini;
+		
+		#configure php to use memcached for sessions
+		sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
+		if grep "^session.save_path" /etc/php5/apache2/php.ini ; then
+		# the line exists
+			sed -i "/^session.save_path/c\session.save_path=\'#{session_save_path}\'" /etc/php5/apache2/php.ini;
+		else
+		#this is a new def
+			echo session.save_path=\'#{session_save_path}\' >> /etc/php5/apache2/php.ini;
+		fi
+		
+		#configure redundancy 
+		if ! grep "memcache\.allow_failover=1" /etc/php5/mods-available/memcache.ini ;  then
+			echo "memcache.allow_failover=1" >> /etc/php5/mods-available/memcache.ini;
+			echo memcache.session_redundancy=#{node[:opsworks][:layers]['php-app'][:instances].length+1} >> /etc/php5/mods-available/memcache.ini;
+		else
+			sed -i "/memcache.session_redundancy/c\  memcache.session_redundancy=#{node[:opsworks][:layers]['php-app'][:instances].length+1}" /etc/php5/mods-available/memcache.ini;
+		fi
+		
 	EOH
 end
