dfcb9e8c351611fbc701fcbb50f64cd2536d9d78
Roumen B. Antonov <roumen@roumen.us>
2015-04-15 14:46:14 -0700

    memcached take 13

--- zendywebhost/recipes/installmemcache.rb
+++ zendywebhost/recipes/installmemcache.rb
@@ -27,7 +27,9 @@ script "installmemcache" do
 		
 	
 		echo '*********** configure php to use memcached for sessions *************'
+		echo sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
 		sed -i "/session.save_handler/c\  session.save_handler = memcache" /etc/php5/apache2/php.ini;
+		
 		if grep "^\ *session.save_path" /etc/php5/apache2/php.ini ; then
 			echo '*********** the line exists *************'
 			sed -i "/^session.save_path/c\ session.save_path=#{session_save_path}" /etc/php5/apache2/php.ini;
