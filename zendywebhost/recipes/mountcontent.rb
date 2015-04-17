658ab10458ceb7a0917c2e686699721fb148593d
Roumen B. Antonov <roumen@roumen.us>
2015-04-17 10:21:49 -0700

    nfs mount using custom json

--- zendywebhost/recipes/mountcontent.rb
+++ zendywebhost/recipes/mountcontent.rb
@@ -2,10 +2,10 @@ script "mountcontent" do
 		interpreter "bash"
 		user "root"
 		code <<-EOH
-		apt-get install nfs-kernel-server;
 			if ! grep "wp-content" -qs /proc/mounts; then 
+				apt-get -y install nfs-common;
 				mkdir -p /srv/www/zh_wordpress/shared/content;
-				mount -t nfs 10.0.1.16:/usr/share/nas/wp-content  /srv/www/zh_wordpress/shared/content
+				mount -t nfs #{node[:deploy][:zh_wordpress][:nfs][:url]}  /srv/www/zh_wordpress/shared/content
 			fi
 		EOH
 end
