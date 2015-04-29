1272115825e584b7630e91e4264a3b1a43d0d89d
Roumen B. Antonov <roumen@roumen.us>
2015-04-28 18:55:56 -0700

    wordpress template changes

--- zendywebhost/recipes/mountcontent.rb
+++ zendywebhost/recipes/mountcontent.rb
@@ -6,6 +6,7 @@ script "mountcontent" do
 				apt-get -y install nfs-common;
 				mkdir -p /srv/www/zh_wordpress/shared/content;
 				mount -t nfs #{node[:deploy][:zh_wordpress][:nfs][:url]}  /srv/www/zh_wordpress/shared/content
+				chmod -R 777 /srv/www/zh_wordpress/shared/content/*
 			fi
 		EOH
 end
