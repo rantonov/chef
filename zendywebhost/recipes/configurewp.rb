c00b90dd88b68c1770e7d77f7fd88e42fea902a9
Roumen B. Antonov <roumen@zendyhealth.com>
2015-05-07 10:18:57 -0700

    Moved the link content to the mount step

--- zendywebhost/recipes/configurewp.rb
+++ zendywebhost/recipes/configurewp.rb
@@ -75,17 +75,6 @@ node[:deploy].each do |app_name, deploy|
 #			owner "root"
 #		end 
 		
-		script "linkconfigs" do
-			interpreter "bash"
-			user "root"
-			code <<-EOH
-					WP_CONTENT=#{deploy[:deploy_to]}/current/wp-content;
-					if ! [[ -L "$WP_CONTENT" && -d "$WP_CONTENT" ]]; then
-						rm -rf $WP_CONTENT;
-						ln -s /srv/www/zh_wordpress/shared/content $WP_CONTENT;
-					fi
-			EOH
-		end		
 	end	
 end
 
--- zendywebhost/recipes/mountcontent.rb
+++ zendywebhost/recipes/mountcontent.rb
@@ -7,6 +7,19 @@ script "mountcontent" do
 				mkdir -p /srv/www/zh_wordpress/shared/content;
 				mount -t nfs #{node[:deploy][:zh_wordpress][:nfs][:url]}  /srv/www/zh_wordpress/shared/content
 				chmod -R 777 /srv/www/zh_wordpress/shared/content/*
+
 			fi
 		EOH
 end
+
+script "linkconfigs" do
+	interpreter "bash"
+	user "root"
+	code <<-EOH
+			WP_CONTENT=#{deploy[:deploy_to]}/current/wp-content;
+			if ! [[ -L "$WP_CONTENT" && -d "$WP_CONTENT" ]]; then
+				rm -rf $WP_CONTENT;
+				ln -s /srv/www/zh_wordpress/shared/content $WP_CONTENT;
+			fi
+	EOH
+end		
