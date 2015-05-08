c0af0614465193b774853c09248c6c524cdbeb0d
Roumen B. Antonov <roumen@zendyhealth.com>
2015-05-07 20:08:41 -0700

    don't remove wp-content files

--- zendywebhost/recipes/mountcontent.rb
+++ zendywebhost/recipes/mountcontent.rb
@@ -5,7 +5,7 @@ script "mountcontent" do
 			if ! grep "wp-content" -qs /proc/mounts; then 
 				apt-get -y install nfs-common;
 				mkdir -p /srv/www/zh_wordpress/shared/content;
-				mount -t nfs #{node[:deploy][:zh_wordpress][:nfs][:url]}  /srv/www/zh_wordpress/shared/content
+				mount -t nfs #{node[:deploy][:zh_wordpress][:nfs][:url]}  /srv/www/zh_wordpress/current/wp-content
 				chmod -R 777 /srv/www/zh_wordpress/shared/content/*
 
 			fi
@@ -20,14 +20,14 @@ end
 #			owner "root"
 #		end 
 
-script "linkconfigs" do
-	interpreter "bash"
-	user "root"
-	code <<-EOH
-			WP_CONTENT=/srv/www/zh_wordpress/current/wp-content;
-			if ! [[ -L "$WP_CONTENT" && -d "$WP_CONTENT" ]]; then
-				rm -rf $WP_CONTENT;
-				ln -s /srv/www/zh_wordpress/shared/content $WP_CONTENT;
-			fi
-	EOH
-end		
+# script "linkconfigs" do
+# 	interpreter "bash"
+# 	user "root"
+# 	code <<-EOH
+# 			WP_CONTENT=/srv/www/zh_wordpress/current/wp-content;
+# 			if ! [[ -L "$WP_CONTENT" && -d "$WP_CONTENT" ]]; then
+# 				rm -rf $WP_CONTENT;
+# 				ln -s /srv/www/zh_wordpress/shared/content $WP_CONTENT;
+# 			fi
+# 	EOH
+# end		
